81ec8840aa9f006791122fa9ede8312c36f22945
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-01-06 17:31:33 +0200

    more consistent code style

--- test/phantom.js
+++ test/phantom.js
@@ -4,32 +4,32 @@ var page = new WebPage;
 page.onConsoleMessage = function(msg) {
     // cursor removed from phantomjs
     if (!/^\s+(?:\\|\/|\||\-)/.test(msg)) {
-        console.log(msg.replace("\n", ""));
+        console.log(msg.replace('\n', ''));
     }
 };
 
 page.open('about:blank', function(status) {
-    if (status != "success") {
+    if (status !== 'success') {
         return phantom.exit();
     }
 
-    page.evaluate(function () {
+    page.evaluate(function() {
         window.phantomExit = false;
-        window.quit = function () {
+        window.quit = function() {
             window.phantomExit = true;
         };
-        window.require = function () {
+        window.require = function() {
             return {wru: window.wru};
         };
         window.global = window;
     });
 
     page.injectJs('keymage.js');
-    page.injectJs("test/vendor/wru.console.max.js");
-    page.injectJs("test/test.js");
+    page.injectJs('test/vendor/wru.console.max.js');
+    page.injectJs('test/test.js');
 
-    setInterval(function () {
-        page.evaluate(function () {
+    setInterval(function() {
+        page.evaluate(function() {
             return window.phantomExit;
         }) && phantom.exit();
     }, 50);
